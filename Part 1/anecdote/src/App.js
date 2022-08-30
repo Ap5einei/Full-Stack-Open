917b1e8db608e408d2beda74bedda8f5dd90d817
Ap5einei <104532688+Ap5einei@users.noreply.github.com>
2022-08-30 03:30:35 +0300

    Update App.js

--- Part 1/anecdote/src/App.js
+++ Part 1/anecdote/src/App.js
@@ -1,62 +1,49 @@
-import React,{useState} from 'react'
+import { useState } from "react";
 
-const Button = ({title,clickAction}) => {
-  return (
-    <button onClick={clickAction}>{title}</button>
-  )
-}
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
-const Heading = ({text}) => {
-  return (
-    <h2>{text}</h2>
-  )
-}
-
-const Paragraph = ({text}) => {
-  return(
-    <p>{text}</p>
-  )
-}
 const App = () => {
-
   const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ]
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+  ];
 
-  const [selected,setSelected] = useState(0)
-  const [vote,setVote] = useState(new Array(6).fill(0));
+  const [selected, setSelected] = useState(0);
 
-  const getnextAnecdote = () => {
-    const pos = Math.floor((Math.random()*anecdotes.length)+0)
-    setSelected(pos)
-  }
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
 
-  const updateVotes = () => {
-    const copy = [...vote]
-    copy[selected]+=1
-    setVote(copy)
-  }
+  const addVote = () => {
+    const pointsCopy = [...points];
+    pointsCopy[selected] += 1;
+    setPoints(pointsCopy);
+  };
 
-  const maxvote = Math.max(...vote)
-  const maxquote = anecdotes[vote.indexOf(maxvote)]
+  const bestAnecdoteIndex = points.indexOf(Math.max(...points));
 
   return (
-      <div>
-      <Heading text="Anecdote of the day" />
-      <Paragraph text={anecdotes[selected]} />
-      <p>has {vote[selected]} votes</p>
-      <Button title="next anecdote" clickAction={getnextAnecdote} />
-      <Button title="vote" clickAction={updateVotes} />
-      <Heading text="Anecdote with the most votes" />
-      <Paragraph text={maxquote} />
-      <p>has {maxvote} votes</p>
-      </div>
+    <div>
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>has {points[selected]} points</div>
+      <Button handleClick={() => addVote()} text="vote" />
+      <Button
+        handleClick={() =>
+          setSelected(Math.floor(Math.random() * anecdotes.length))
+        }
+        text="next anecdote"
+      />
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[bestAnecdoteIndex]}</div>
+      <div>has {points[bestAnecdoteIndex]} votes</div>
+    </div>
   );
-}
+};
 
 export default App;
